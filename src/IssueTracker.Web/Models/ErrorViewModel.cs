cc745590c96b982c16dd34452d49d88cb81ad20f
kborys <konrad.borys@outlook.com>
2024-02-24 12:58:07 +0100

    Init

---
+++ src/IssueTracker.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace IssueTracker.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
